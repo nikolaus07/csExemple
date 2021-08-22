bd4647dd673b2f5022f0db5b3baecbff950c62c7
nikolaus07 <claus@thevessen.ch>
2021-08-22 16:57:00 +0200

    add Nlog_Beispiel

---
+++ StartMain_Nunit.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace csExemple
+{
+    class StartMain_Nunit
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void test_N_log()
+        {
+            Test_N_Log.testLogger();
+            Assert.Pass();
+
+            // output yyyy / mm / dd 10:37:21.518 | Info | das ist INFO-logger
+            // output yyyy / mm / dd 10:37:21.587 | Warn | das ist WARN-logger
+            // output yyyy / mm / dd 10:37:21.587 | Error | das ist ERROR-logger
+            // output yyyy / mm / dd 10:37:21.587 | Fatal | das ist FATAL-logger
+            // output yyyy / mm / dd 16:00:07.972 | Info | das ist INFO-logger
+            // output yyyy / mm / dd 16:00:08.047 | Warn | das ist WARN-logger
+            // output yyyy / mm / dd 16:00:08.169 | Error | das ist ERROR-logger
+            // output yyyy / mm / dd 16:00:08.169 | Fatal | das ist FATAL-logger
+        }
+    }
+
+}
+
---
+++ Test_N_Log.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+
+namespace csExemple
+{
+    class Test_N_Log
+    {
+        private static readonly ILogger _NLogger = LogManager.GetCurrentClassLogger();
+
+
+        public static void testLogger()
+        {
+            _NLogger.Info("das ist INFO-logger");
+            _NLogger.Warn("das ist WARN-logger");
+            _NLogger.Error("das ist ERROR-logger");
+            _NLogger.Fatal("das ist FATAL-logger");
+        }
+
+    }
+}
